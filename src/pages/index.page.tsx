8d179508c3e9b0d7bab72f918609174ceeda78ba
jttrevis <jr.trevis@hotmail.com>
2023-06-26 16:16:06 +0100

    eslint and libs added

---
+++ src/pages/index.page.tsx
@@ -0,0 +1,11 @@
+import { Heading, Text } from '@ignite-ui/react'
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <Heading as="h1">hello</Heading>
+      </main>
+    </>
+  )
+}
