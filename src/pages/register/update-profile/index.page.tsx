d2b913aa17b7c17e2d2b76293767feed1147b8e7
jttrevis <jr.trevis@hotmail.com>
2023-07-06 22:42:26 +0100

    GetServerSideContext

--- src/pages/register/update-profile/index.page.tsx
+++ src/pages/register/update-profile/index.page.tsx
@@ -1,44 +1,42 @@
-import React from 'react'
-import { Button, Heading, MultiStep, Text, TextArea } from '@ignite-ui/react'
-import { ArrowRight } from 'phosphor-react'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Container, Header } from '../styles'
-import { FromAnnotations, ProfileBox } from './style'
-import { useSession } from 'next-auth/react'
+import React from "react";
+import { Button, Heading, MultiStep, Text, TextArea } from "@ignite-ui/react";
+import { ArrowRight } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Container, Header } from "../styles";
+import { FromAnnotations, ProfileBox } from "./style";
+import { useSession } from "next-auth/react";
+import { GetServerSideProps } from "next";
+import { getServerSession } from "next-auth";
+import { buildNextAuthOptions } from "@/pages/api/auth/[...nextauth].api";
 
 const updateProfileSchema = z.object({
   bio: z.string(),
-})
+});
 
-type UpdateProfileData = z.infer<typeof updateProfileSchema>
+type UpdateProfileData = z.infer<typeof updateProfileSchema>;
 
 export default function UpdateProfile() {
   const {
     register,
     handleSubmit,
 
-    formState: {  isSubmitting },
+    formState: { isSubmitting },
   } = useForm<UpdateProfileData>({
-    resolver: zodResolver(updateProfileSchema)
-  })
+    resolver: zodResolver(updateProfileSchema),
+  });
 
-
-  const session = useSession()
+  const session = useSession();
 
   console.log(session);
-  
 
-
-  const handleUpdateProfile = async (data: UpdateProfileData) => {
-
-  }
+  const handleUpdateProfile = async (data: UpdateProfileData) => {};
 
   return (
     <Container>
       <Header>
-        <Heading as={'strong'}>Bem-vindo ao Ignite Call!</Heading>
+        <Heading as={"strong"}>Bem-vindo ao Ignite Call!</Heading>
 
         <Text>
           Precisamos de algumas informações para criar seu perfil! Ah, você pode
@@ -48,25 +46,40 @@ export default function UpdateProfile() {
         <MultiStep size={4} currentStep={4} />
       </Header>
 
-      <ProfileBox as={'form'} onSubmit={handleSubmit(handleUpdateProfile)}>
+      <ProfileBox as={"form"} onSubmit={handleSubmit(handleUpdateProfile)}>
         <label htmlFor="">
-          <Text size={'sm'}>Foto de perfil</Text>
+          <Text size={"sm"}>Foto de perfil</Text>
         </label>
 
-
         <label>
-          <Text size={'sm'}>Sobre voce</Text>
-          <TextArea {...register('bio')} />
-          <FromAnnotations size={'sm'}>
+          <Text size={"sm"}>Sobre voce</Text>
+          <TextArea {...register("bio")} />
+          <FromAnnotations size={"sm"}>
             Fale um pouco sobre voce. Isto sera exibido em sua pagina pessoal
           </FromAnnotations>
         </label>
-     
+
         <Button type="submit" disabled={isSubmitting}>
           Finalizar
           <ArrowRight />
         </Button>
       </ProfileBox>
     </Container>
-  )
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = await getServerSession(
+    req,
+    res,
+    buildNextAuthOptions(req, res),
+  )
+
+
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
